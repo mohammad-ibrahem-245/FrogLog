d4ffe1e1ce7664f7e237d8e2736f2adbadf1399a
mohammadali <mohammadibrahem245@gmail.com>
2025-09-23 04:10:00 +0400

    added the rest of apis with their dockerfiles

---
+++ friendship-api/src/main/java/org/example/friendshipapi/FriendshipApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.friendshipapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendshipApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendshipApiApplication.class, args);
+    }
+
+}
