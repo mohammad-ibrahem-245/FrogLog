dd67a005ece658f2a0306b2f60d003bc639069e2
mohammadali <mohammadibrahem245@gmail.com>
2025-10-05 03:24:32 +0400

    first version of Project api

---
+++ Project-api/src/main/java/org/example/projectapi/Model/Project.java
@@ -0,0 +1,31 @@
+package org.example.projectapi.Model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.scheduling.config.Task;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false , unique = true )
+    private String name;
+    private String description;
+    @ElementCollection
+    private List<String> members;
+    private List<Long> tasks;
+    private LocalDate createdDate;
+    @Column(nullable = false)
+    private String owner;
+}
